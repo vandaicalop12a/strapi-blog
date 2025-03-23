0c3a9bad26c6cd1fc452fd2f519561fd382d813f
Văn <van.nguyen@zen8labs.com>
2025-03-23 12:39:50 +0700

    query post by slug

--- src/api/post/services/post.js
+++ src/api/post/services/post.js
@@ -1,9 +1,14 @@
-'use strict';
+"use strict";
 
 /**
  * post service
  */
 
-const { createCoreService } = require('@strapi/strapi').factories;
+const { createCoreService } = require("@strapi/strapi").factories;
 
-module.exports = createCoreService('api::post.post');
+module.exports = createCoreService("api::post.post", ({ strapi }) => ({
+  findOne(slug, params = {}) {
+    const { uid } = this.contentType;
+    return strapi.db.query(uid).findOne({ where: { slug }, ...params });
+  },
+}));
